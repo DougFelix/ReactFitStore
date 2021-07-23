52c097e55b5588d413f7707227a8a8aa3659385a
Douglas Freitas <dotfreitas@gmail.com>
2021-07-23 19:28:52 -0300

    First commit - CRA and axios

---
+++ src/Components/ItemList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Item from './Item';
+import axios from 'axios';
+
+class ItemList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ();
+    }
+}
+ 
+export default ItemList;
---
+++ src/Components/Store.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Navbar from './Navbar';
+import Cart from './Cart';
+import ItemList from './ItemList';
+
+class Store extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Store;
